b94d7cacfecd7ab0233c44086d6dac780fe62287
yazan <yazanalshabie786@gmail.com>
2024-05-08 14:08:30 +0300

    front merge

--- resources/views/front/Order.blade.php
+++ resources/views/front/Order.blade.php
@@ -1,33 +1,32 @@
 @extends('front.master')
 
 @section('content')
-    <section class="ls section_padding_120 team_member">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-5 col-md-5 col-sm-12 text-center bottommargin_30">
-                    <div class="vertical-item title-absolute">
-                        <div class="item-media team_member_photo">
-                            <img src="{{ asset('storage/' . $maid->image) }}" alt="">
-                        </div>
-                    </div>
-                    <div class="item-content with_padding text-center">
-                        <h2 class="topmargin_-5 bottommargin_25">
-                            {{ $maid->first_name }} <strong>{{ $maid->last_name }}</strong>
-                        </h2>
+<section class="ls section_padding_120 team_member">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-md-5 col-sm-12 text-center bottommargin_30">
+                <div class="vertical-item title-absolute">
+                    <div class="item-media team_member_photo">
+                        <img src="{{ asset('storage/' . $maid->image) }}" alt="" style="max-width: 100%; height: auto;">
                     </div>
                 </div>
-                <div class="col-lg-7 col-md-7 col-sm-12 text-center bottommargin_30" style="    padding-top: 101px;">
-                    <div class="vertical-item title-absolute">
-                        <div class="item-media team_member_video">
-                            <!-- Here's the video -->
-                            <iframe width="100%" height="315" src="{{ asset('storage/' . $maid->video) }}"
-                                frameborder="0" allowfullscreen></iframe>
-                        </div>
+                <div class="item-content with_padding text-center">
+                    <h2 class="topmargin_-5 bottommargin_25">
+                        {{ $maid->first_name }} <strong>{{ $maid->last_name }}</strong>
+                    </h2>
+                </div>
+            </div>
+            <div class="col-lg-7 col-md-7 col-sm-12 text-center bottommargin_30" style="padding-top: 101px;">
+                <div class="vertical-item title-absolute">
+                    <div class="item-media team_member_video">
+                        <!-- Here's the video -->
+                        <iframe width="100%" height="315" src="{{ asset('storage/' . $maid->video) }}" frameborder="0" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
 <!-- المحتوى الجديد -->
 <section class="ls section_padding_120">
@@ -56,49 +55,48 @@
                             </ul>
                         </p>
                         <!-- زر الطلب -->
-                        <!-- <a href="login" id="orderButton" class="theme_button muted_button">Order</a> -->
                         <a href="#" id="orderButton" class="theme_button muted_button">أطلب الان</a>
                     </div>  
                 </div><!-- eof .tab-content -->
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <!-- الجزء الآخر من المحتوى -->
-    <section class="ds solutions columns_padding_0 teasers-table">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 center_bg users-teaser">
-                    <div class="teaser media">
-                        <div class="media-left">
-                            <div class="teaser_icon sprite-icons sprite-users-yellow"></div>
-                        </div>
-                        <div class="media-body">
-                            <p class="margin_0 small-text grey"><strong>Highly-trained staff</strong></p>
-                            <p class="margin_0">Sed ut perspiciatis, unde omnis is natus error sit voluptatem</p>
-                        </div>
+<!-- الجزء الآخر من المحتوى -->
+<section class="ds solutions columns_padding_0 teasers-table">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 center_bg users-teaser">
+                <div class="teaser media">
+                    <div class="media-left">
+                        <div class="teaser_icon sprite-icons sprite-users-yellow"></div>
+                    </div>
+                    <div class="media-body">
+                        <p class="margin_0 small-text grey"><strong>Highly-trained staff</strong></p>
+                        <p class="margin_0">Sed ut perspiciatis, unde omnis is natus error sit voluptatem</p>
                     </div>
                 </div>
-                <div class="col-md-4 center_bg like-teaser">
-                    <div class="teaser media with_border">
-                        <div class="media-left">
-                            <div class="teaser_icon sprite-icons sprite-like-green"></div>
-                        </div>
-                        <div class="media-body">
-                            <p class="margin_0 small-text grey"><strong>Quality cleaning tools</strong></p>
-                            <p class="margin_0">Doloremque laudantium, totam aperiam eaque ipsa, quae abillo</p>
-                        </div>
+            </div>
+            <div class="col-md-4 center_bg like-teaser">
+                <div class="teaser media with_border">
+                    <div class="media-left">
+                        <div class="teaser_icon sprite-icons sprite-like-green"></div>
+                    </div>
+                    <div class="media-body">
+                        <p class="margin_0 small-text grey"><strong>Quality cleaning tools</strong></p>
+                        <p class="margin_0">Doloremque laudantium, totam aperiam eaque ipsa, quae abillo</p>
                     </div>
                 </div>
-                <div class="col-md-4 center_bg timer-teaser">
-                    <div class="teaser media">
-                        <div class="media-left">
-                            <div class="teaser_icon sprite-icons sprite-timer-blue"></div>
-                        </div>
-                        <div class="media-body">
-                            <p class="margin_0 small-text grey"><strong>Fast &amp; Effective service</strong></p>
-                            <p class="margin_0">Voluptatem accusantium doloremque laudantium totam</p>
-                        </div>
+            </div>
+            <div class="col-md-4 center_bg timer-teaser">
+                <div class="teaser media">
+                    <div class="media-left">
+                        <div class="teaser_icon sprite-icons sprite-timer-blue"></div>
+                    </div>
+                    <div class="media-body">
+                        <p class="margin_0 small-text grey"><strong>Fast &amp; Effective service</strong></p>
+                        <p class="margin_0">Voluptatem accusantium doloremque laudantium totam</p>
                     </div>
                 </div>
             </div>
